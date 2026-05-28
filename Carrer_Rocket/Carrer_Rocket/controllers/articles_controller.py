75a0f6671be65d81cefdf70166bcb7bb9d7d5602
SpunchBob <aleksandrprokudin2007@gmail.com>
2026-05-28 12:22:37 +0300

    fix(): fixing article_controller

--- Carrer_Rocket/Carrer_Rocket/controllers/articles_controller.py
+++ Carrer_Rocket/Carrer_Rocket/controllers/articles_controller.py
@@ -8,6 +8,7 @@ from services.validation_service import ValidationError
 def articles_index():
     start_date = request.query.get('start_date', '')
     end_date = request.query.get('end_date', '')
+    scroll_to = request.query.get('scroll_to', '')
     
     try:
         if start_date and end_date:
@@ -27,6 +28,7 @@ def articles_index():
         form_data={},
         start_date=start_date,
         end_date=end_date,
+        scroll_to=scroll_to,
         title='Статьи',
         year=2026
     )
@@ -48,6 +50,7 @@ def articles_by_date(postDate):
         form_data={},
         start_date=postDate,
         end_date=postDate,
+        scroll_to='',
         title=f'Статьи за {postDate}',
         year=2026
     )
@@ -75,6 +78,7 @@ def articles_by_interval():
         form_data={},
         start_date=start_date,
         end_date=end_date,
+        scroll_to='',
         title=f'Статьи с {start_date} по {end_date}',
         year=2026
     )
@@ -91,7 +95,15 @@ def articles_add():
     }
     
     try:
-        ArticleService.add_new_article(form_data)
+        new_article = ArticleService.add_new_article(form_data)
+        articles = ArticleService.get_all_articles()
+        articles.sort(key=lambda x: x.postDate, reverse=True)
+        
+        for idx, article in enumerate(articles):
+            if article.header == form_data['header'] and article.postDate == form_data['postDate']:
+                redirect(f'/articles?scroll_to=article_{idx}')
+                return
+        
         redirect('/articles')
     except ValidationError as e:
         articles = ArticleService.get_all_articles()
@@ -103,6 +115,7 @@ def articles_add():
             form_data=form_data,
             start_date='',
             end_date='',
+            scroll_to='',
             title='Статьи',
             year=2026
         )
@@ -114,8 +127,8 @@ def articles_filter():
     end_date = request.forms.get('end_date', '').strip()
     
     if start_date and end_date:
-        redirect(f'/articles/interval?start_date={start_date}&end_date={end_date}')
+        redirect(f'/articles/interval?start_date={start_date}&end_date={end_date}&scroll_to=articles_list')
     elif start_date and not end_date:
-        redirect(f'/articles/date/{start_date}')
+        redirect(f'/articles/date/{start_date}?scroll_to=articles_list')
     else:
         redirect('/articles')
